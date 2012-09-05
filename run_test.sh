e18e9533a724bfd841fda980a92555983f770989
xd <dxj19831029@gmail.com>
2012-09-05 18:49:29 +0800

    test running script.

---
+++ run_test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+bacon spec/librevox/spec_response.rb 
+bacon spec/librevox/listener.rb 
+bacon spec/librevox/spec_applications.rb 
+bacon spec/librevox/spec_commands.rb 
+bacon spec/librevox/listener/spec_outbound.rb
